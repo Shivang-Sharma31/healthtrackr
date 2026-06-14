97f7dbba8e6142beb15bab060a3d864bed156fe9
Shivang Sharma <shivang.ganotra31@gmail.com>
2026-06-14 23:02:00 +0530

    refreshAccessToken

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -13,4 +13,41 @@ const logoutUser = asyncHandler(async (req,res) => {
 
 })
 
-export { registerUser,loginUser,logoutUser };
+
+const refreshAccessToken = asyncHandler(async(req,res)=>{
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken ;
+    if(!incomingRefreshToken) {
+        throw new ApiError (401 , "Refresh token required");
+    }
+
+    try {
+        jwt.verify(
+            incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET
+        )
+        const user = await User.findById(decodedToken?._id);
+        if(!user) throw new ApiError (404 , "Invalid Refresh token");
+
+        if(incomingRefreshToken !== user?.refreshToken ){
+            throw new ApiError (401 , "Invalid Refresh token");
+        }
+
+        const options = {
+            httpOnly :true,
+            secure : process.env.NODE_ENV === "production",
+        }
+        const {accessToken , refreshToken : newRefreshToken} = await generateAcessAndRefreshToken (user._id);
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(new ApiResponse(200, {accessToken,refreshToken:newRefreshToken}," access token refreshed successfully :)"));
+
+
+    } catch (error) {
+        throw new ApiError (500, "something went wrong while refreshing access token");
+    }
+
+})
+
+export { registerUser,loginUser,logoutUser,refreshAccessToken };
